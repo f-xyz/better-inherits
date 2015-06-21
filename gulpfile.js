19cbabd6514585ba21f48502146a7c12fa78fdc0
f-xyz <andrey.lischina@gmail.com>
2015-06-21 21:45:30 +0300

    browserified bundle now creates global

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var bump = require('gulp-bump');
 var mocha = require('gulp-spawn-mocha');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
@@ -7,55 +8,67 @@ var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var del = require('del');
 var browserify = require('browserify');
-var async = require('async');
+var _ = require('lodash');
+
+function getBundleName(ext) {
+    var pkg = require('./package.json');
+    return pkg.name + '-' + pkg.version + ext;
+}
 
 gulp.task('default', ['build']);
-gulp.task('build', ['clean', 'browserify', 'test']);
-
-gulp.task('clean', function(cb) {
-    async.parallel([
-        function(next) { del('dist/*.*', next) },
-        function(next) { del('coverage/', next) },
-        function(next) { del('docs/', next) }
+gulp.task('build', ['clean', 'bump', 'browserify', 'test']);
+gulp.task('clean', function (cb) {
+    del([
+        'dist/',
+        'coverage/',
+        'docs/'
     ], cb);
 });
-
-gulp.task('browserify', function() {
+gulp.task('bump', function () {
+    return gulp
+        .src('./package.json')
+        .pipe(bump({ type: 'build-version' }))
+        .pipe(gulp.dest('./'));
+});
+gulp.task('browserify', ['bump'], function () {
     var bundler = browserify({
         entries: ['./index.js'],
         debug: true,
-        detectGlobals: false,
+        detectGlobals: true,
         insertGlobals: false,
-        standalone: 'inherits'
+        standalone: _.camelCase(require('./package.json').name)
     });
     return bundler
         .bundle()
-
         .pipe(source(getBundleName('.js')))
         .pipe(gulp.dest('dist/'))
-
+        //
         .pipe(rename({ extname: '.min.js' }))
         .pipe(buffer())
         .pipe(sourcemaps.init({ loadMaps: true }))
         .pipe(uglify())
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('dist/'))
-    ;
+        .pipe(gulp.dest('dist/'));
 });
-
-gulp.task('test', function() {
-    return gulp.src('test/*.*', { read: false })
+gulp.task('test', function () {
+    return gulp.src('tests/index.js', { read: false })
         .pipe(mocha({
             R: 'spec',
             colors: true,
+            watch: false,
             debug: true,
             istanbul: true,
             compilers: 'coffee:coffee-script/register'
         }));
 });
-
-function getBundleName(ext) {
-    var name = require('./package.json').name;
-    var version = require('./package.json').version;
-    return name + '-' + version + ext;
-}
+gulp.task('test-watch', function () {
+    return gulp.src('tests/index.js', { read: false })
+        .pipe(mocha({
+            R: 'spec',
+            colors: true,
+            watch: true,
+            debug: true,
+            istanbul: true,
+            compilers: 'coffee:coffee-script/register'
+        }));
+});
